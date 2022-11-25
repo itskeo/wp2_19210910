71b20c7fbfde086738fdbce0c7e099ecba0228e6
itskeo <keona.adhitya@gmail.com>
2022-11-25 20:20:00 +0700

    Modifikasi Sidebar dan Membuat CRUD Data Kategori Buku

--- application/models/ModelBukuu.php
+++ application/models/ModelBukuu.php
@@ -32,7 +32,6 @@ class ModelBuku extends CI_Model
         $this->db->from('buku');
         return $this->db->get()->row($field);
     }
-
     //manajemen kategori
     public function getKategori()
     {
@@ -59,8 +58,7 @@ class ModelBuku extends CI_Model
     {
         $this->db->select('buku.id_kategori,kategori.kategori');
         $this->db->from('buku');
-        $this->db->join('kategori', 'kategori.id = 
-buku.id_kategori');
+        $this->db->join('kategori', 'kategori.id = buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
     }
